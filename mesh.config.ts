766d63a8d0eff8f1e9fb9e0feabb623f821b729a
msolorio <msolorio.github@gmail.com>
2024-09-22 20:09:55 -0700

    Serve todos rest api with graphql mesh

--- mesh.config.ts
+++ mesh.config.ts
@@ -1,11 +1,40 @@
-import { defineConfig, loadGraphQLHTTPSubgraph } from '@graphql-mesh/compose-cli'
+// import { defineConfig, loadGraphQLHTTPSubgraph } from '@graphql-mesh/compose-cli'
+import { defineConfig, } from '@graphql-mesh/compose-cli'
+import { loadJSONSchemaSubgraph } from '@omnigraph/json-schema'
+import { OperationTypeNode } from 'graphql'
+
+// export const composeConfig = defineConfig({
+//   subgraphs: [
+//     {
+//       sourceHandler: loadGraphQLHTTPSubgraph('Countries', {
+//         endpoint: 'https://countries.trevorblades.com'
+//       })
+//     }
+//   ]
+// })
 
 export const composeConfig = defineConfig({
   subgraphs: [
     {
-      sourceHandler: loadGraphQLHTTPSubgraph('Countries', {
-        endpoint: 'https://countries.trevorblades.com'
-      })
-    }
-  ]
-})
+      sourceHandler: loadJSONSchemaSubgraph('todoApi', {
+        endpoint: `http://localhost:3000`,
+        operationHeaders: {
+          'Content-Type': 'application/json',
+        },
+        operations: [
+          {
+            type: OperationTypeNode.QUERY,
+            field: 'todos',
+            path: '/todos',
+            method: 'GET',
+            responseSample: './gql_schemas/todos.json',
+          },
+        ],
+      }),
+    },
+  ],
+  // additionalTypeDefs: /* GraphQL */ `
+  //   directive @live on QUERY
+  // `,
+});
+
--- src/app/entrypoints/index.ts
+++ src/app/entrypoints/index.ts
@@ -7,8 +7,17 @@ const app = express()
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
+const todos = [
+  { id: 1, name: 'todo1', content: 'todoContent1' },
+  { id: 2, name: 'todo2', content: 'todoContent2' },
+]
+
 app.get('/health', (_, res) => {
   res.status(200).send('OK')
+})
+
+app.get('/todos', (_, res) => {
+  res.json(todos)
 })
 
 app.listen(getPort(), () => {
